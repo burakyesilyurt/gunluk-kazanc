9c9a8a7d33f0d64a1d5351591977976f2b8e6d50
Burak Yesilyurt <yesilyurt.burak07@gmail.com>
2023-05-24 00:11:31 +0300

    navbar href changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,49 +16,61 @@
       @if(Auth::User()->role == 1)
       <div class="mt-32 flex justify-center flex-col gap-12">
         <div>
-          <div class="card w-96 bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-              <a href="/profil/{{Auth::User()->id}}"> <span class="card-title text-2xl">Profil</span></a>
+          <a href="/profil/{{Auth::User()->id}}">
+            <div class="card w-96 bg-neutral text-neutral-content">
+              <div class="card-body items-center text-center">
+                <span class="card-title text-2xl">Profil</span>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
         <div>
-          <div class="card w-96 bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-              <a href="{{route('profil')}}"><span class="card-title text-2xl">Profili Güncelle</span></a>
+          <a href="{{route('profil')}}">
+            <div class="card w-96 bg-neutral text-neutral-content">
+              <div class="card-body items-center text-center">
+                <span class="card-title text-2xl">Profili Güncelle</span>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
         <div>
-          <div class="card w-96 bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-              <a href="{{route('basvuranlar')}}"><span class="card-title text-2xl">Başvuranlar</span></a>
+          <a href="{{route('ilanlar')}}">
+            <div class="card w-96 bg-neutral text-neutral-content">
+              <div class="card-body items-center text-center">
+                <span class="card-title text-2xl">İlanlar</span>
+              </div>
             </div>
-          </div>
         </div>
+        </a>
       </div>
       @elseif (Auth::User()->role == 2)
       <div class="mt-32 flex justify-center flex-col gap-12">
         <div>
-          <div class="card w-96 bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-              <a href="{{route('ilanlarim')}}"> <span class="card-title text-2xl">İlanlarım</span></a>
+          <a href="{{route('ilanlarim')}}">
+            <div class="card w-96 bg-neutral text-neutral-content">
+              <div class="card-body items-center text-center">
+                <span class="card-title text-2xl">İlanlarım</span>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
         <div>
-          <div class="card w-96 bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-              <a href="{{route('ilanver')}}"><span class="card-title text-2xl">İlan Ver</span></a>
+          <a href="{{route('ilanver')}}">
+            <div class="card w-96 bg-neutral text-neutral-content">
+              <div class="card-body items-center text-center">
+                <span class="card-title text-2xl">İlan Ver</span>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
         <div>
-          <div class="card w-96 bg-neutral text-neutral-content">
-            <div class="card-body items-center text-center">
-              <a href="{{route('basvuranlar')}}"><span class="card-title text-2xl">Başvuranlar</span></a>
+          <a href="{{route('basvuranlar')}}">
+            <div class="card w-96 bg-neutral text-neutral-content">
+              <div class="card-body items-center text-center">
+                <span class="card-title text-2xl">Başvuranlar</span>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
 
       </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
           @endif
           @else
           <li>
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="/home" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
               {{ Auth::user()->name }}
             </a>
 
