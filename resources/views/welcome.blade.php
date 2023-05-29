129e526bcde0e9b0b730aeba3409d6ba43674944
Burak Yesilyurt <yesilyurt.burak07@gmail.com>
2023-05-30 00:04:17 +0300

    fixed theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,65 +1,73 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html data-theme="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Home Page</title>
+    <title>Home Page</title>
 
 
-  @vite('resources/css/app.css')
+    @vite('resources/css/app.css')
 
 </head>
+
 <body>
-  <div class="hero min-h-[65vh] bg-no-repeat bg-cover bg-center
+    <div
+        class="hero min-h-[65vh] bg-no-repeat bg-cover bg-center
           bg-[url('https://picsum.photos/800/600')]
           xl:bg-[url('https://picsum.photos/1920/1080')]">
-    <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content">
-      <div class="max-w-4xl">
-        <h1 class="mb-5 text-5xl font-bold text-slate-300">Hoş Geldiniz!</h1>
-        <p class="mb-5 text-2xl text-slate-300">
-          İşverenler ve iş arayanlar arasında bağlantı kurmanıza yardımcı olan bir platformuz. Yetenekli iş arayanlarla iletişim kurabilir, iş ilanlarınızı yayınlayabilir ve ekibinizi güçlendirebilirsiniz. Aynı şekilde, farklı sektörlerdeki iş ilanlarını keşfedebilir, başvurularınızı gönderebilir ve kariyerinizi ilerletecek fırsatları yakalayabilirsiniz. Size özel profilinizi oluşturun ve iş dünyasında adınızı duyurun.
-          Hemen kaydolun ve iş dünyasında başarıya doğru adım atın!</p>
-        <div class="flex gap-7 justify-center mt-12">
-          <a href="{{ url('/register') }}">
-            <button for="kayit" class="btn">Kayıt Ol</button></a>
-          <a href="{{ url('/login') }}">
-            <button for="giris" class="btn">Giriş Yap</button>
-          </a>
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-4xl">
+                <h1 class="mb-5 text-5xl font-bold text-slate-300">Hoş Geldiniz!</h1>
+                <p class="mb-5 text-2xl text-slate-300">
+                    İşverenler ve iş arayanlar arasında bağlantı kurmanıza yardımcı olan bir platformuz. Yetenekli iş
+                    arayanlarla iletişim kurabilir, iş ilanlarınızı yayınlayabilir ve ekibinizi güçlendirebilirsiniz.
+                    Aynı şekilde, farklı sektörlerdeki iş ilanlarını keşfedebilir, başvurularınızı gönderebilir ve
+                    kariyerinizi ilerletecek fırsatları yakalayabilirsiniz. Size özel profilinizi oluşturun ve iş
+                    dünyasında adınızı duyurun.
+                    Hemen kaydolun ve iş dünyasında başarıya doğru adım atın!</p>
+                <div class="flex gap-7 justify-center mt-12">
+                    <a href="{{ url('/register') }}">
+                        <button for="kayit" class="btn">Kayıt Ol</button></a>
+                    <a href="{{ url('/login') }}">
+                        <button for="giris" class="btn">Giriş Yap</button>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <div class="mt-12">
-    <div class="ml-44 mb-12">
-      @if ($works->isEmpty())
-      <span class="text-3xl font-medium">Güncel İş İlanı Bulunmamakta</span>
-      @else
-      <span class="text-3xl font-medium">En Güncel İlanlar</span>
-      @endif
+    <div class="mt-12">
+        <div class="ml-44 mb-12">
+            @if ($works->isEmpty())
+                <span class="text-3xl font-medium">Güncel İş İlanı Bulunmamakta</span>
+            @else
+                <span class="text-3xl font-medium">En Güncel İlanlar</span>
+            @endif
 
 
-    </div>
-    <div class="grid sm:grid-cols-1 gap-y-11 justify-items-center xl:grid-cols-3">
-      @foreach ($works as $work )
-      <div class="card w-96 bg-gray-900 shadow-xl 2xl:w-[32rem]">
-        <div class="card-body">
-          <h2 class="card-title font-medium">{{$work->baslik}}</h2>
-          <p class="overflow-hidden max-h-24">{{$work->aciklama}}</p>
-          <div class="card-actions justify-between pt-6 items-center">
-            <div>
-              <span>Başvuru Sayısı {{$work->basvuru_sayisi}}</span>
-            </div>
-            <div>
-              <span class="btn btn-primary cursor-default">{{$work->sehir}}</span>
-              <a href="{{ route('ilanlar') }}" class="btn">İlanlara git</a>
-            </div>
-          </div>
         </div>
-      </div>
-      @endforeach
+        <div class="grid sm:grid-cols-1 gap-y-11 justify-items-center xl:grid-cols-3">
+            @foreach ($works as $work)
+                <div class="card w-96 bg-gray-900 shadow-xl 2xl:w-[32rem]">
+                    <div class="card-body">
+                        <h2 class="card-title font-medium">{{ $work->baslik }}</h2>
+                        <p class="overflow-hidden max-h-24">{{ $work->aciklama }}</p>
+                        <div class="card-actions justify-between pt-6 items-center">
+                            <div>
+                                <span>Başvuru Sayısı {{ $work->basvuru_sayisi }}</span>
+                            </div>
+                            <div>
+                                <span class="btn btn-primary cursor-default">{{ $work->sehir }}</span>
+                                <a href="{{ route('ilanlar') }}" class="btn">İlanlara git</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-  </div>
 </body>
+
 </html>
